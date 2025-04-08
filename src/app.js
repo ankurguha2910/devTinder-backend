fcf3cd269d8b590fa8d0135269a483e1813d6b44
ankurguha2910 <ankur.guha16@gmail.com>
2025-04-08 22:37:47 +0530

    Handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 
-const app = new express();
+const app = express();
+
+app.use("/user", (req, res, next) => {
+    console.log("1st route handler");
+    next();
+    //res.send("1st response");
+}, (req, res, next) => {
+    console.log("2nd route handler");
+    //res.send("2nd response");
+    next();
+}, (req, res, next) => {
+    console.log("3rd route handler");
+    //res.send("3rd response");
+    next();
+}, (req, res, next) => {
+    console.log("4th route handler");
+    //res.send("4th response"); // If we comment this res.send() statement, it will thrown an error because Express.js will be expecting one more route handler that sends the response back to the client. Since no such route handler is defined although we have called the next() function, it gives the error Cannot GET /user
+    next();
+}, (req, res, next) => {
+    console.log("5th route handler");
+    res.send("5th response");
+});
 
 // The callback functions defined inside each of the app.use() corresponding to the paths are known as request handler
 // The following endpoint will handle the GET call to the path beginning with any string but ending with fly
@@ -13,7 +34,7 @@ app.get(/.*fly$/, (req, res) => {
 
 //The following endpoint will only handle the GET call to the /user
 app.get("/user", (req, res) => {
-    //To get the query parameter from the request URL we use object req.query
+    //To get the query parameter from the request we use object req.query
     console.log(req.query);
     res.send({
         firstName: "Ankur",
