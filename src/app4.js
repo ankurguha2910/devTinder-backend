0176fa1df1f5a5e68a85d082795a8c47823fec33
ankurguha2910 <ankur.guha16@gmail.com>
2025-04-09 19:26:32 +0530

    wild card error handling

---
+++ src/app4.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (err, req, res, next) => {
+    if(err)
+    {
+        res.status(500).send("Some error occoured");
+    }
+})
+
+app.use("/getUserData", (req, res) => {
+    throw new Error("hgfhjghj");
+    res.send("User details sent successfully");
+})
+
+//wild card error handling - shold always be written at the end
+app.use("/", (err, req, res, next) => {
+    if(err)
+    {
+        res.status(500).send("Something went wrong");
+    }
+})
+
+app.listen(6000, () => {
+    console.log("Server listening on 6000...");
+})
