3c31767918f25daec9a4803367f688ed0104d37f
ankurguha2910 <ankur.guha16@gmail.com>
2025-04-28 17:22:02 +0530

    GET /feed API with pagination using .skip() and .limit() filters

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userAuth } = require('../middleware/auth');
 const ConnectionRequestModel = require('../models/connectionRequest');
+const { User } = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -39,5 +40,40 @@ userRouter.get("/user/connections" , userAuth, async (req, res) => {
     } catch (error) {
         res.status(400).json({message: "ERROR : "+error.message});
     }
+})
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedinUser = req.user;
+        const page = req.query.page || 1;
+        let limit = req.query.limit || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page -1)*limit;
+        //Find all connection that have been (sent + received)
+        const data = await ConnectionRequestModel.find({
+            $or: [
+                {fromUserId: loggedinUser._id},
+                {toUserId: loggedinUser._id}
+            ]
+        }).select(["fromUserId", "toUserId"]);
+
+        const hideFromFeed = new Set();
+        //add elements to the Set for keeping unique elements in string format
+        data.forEach((row) => {
+            hideFromFeed.add(row.fromUserId.toString());
+            hideFromFeed.add(row.toUserId.toString());
+        });
+        
+        const users =  await User.find({
+            $and: [
+                {_id: { $nin: Array.from(hideFromFeed) }},
+                {_id: { $ne: loggedinUser._id }}
+            ]
+        }).select(["firstName", "lastName", "age", "skills", "about", "photoURL"]).skip(skip).limit(limit);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(400).json({message: "ERROR : "+error.message});
+    }
+    
 })
 module.exports = { userRouter }
